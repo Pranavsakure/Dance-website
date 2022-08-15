786984e41ae81671a1deafd44ee232a4f0341afe
Pranavsakure <105531423+Pranavsakure@users.noreply.github.com>
2022-08-15 08:05:51 +0530

    initial upload

---
+++ app.js
@@ -0,0 +1,64 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+const port = 8000;
+const mongoose = require('mongoose');
+const bodyparser = require("body-parser");
+
+main().catch(err => console.log(err));
+
+async function main() {
+  await mongoose.connect('mongodb://localhost:27017/contactDance');
+}
+
+const contactSchema = new mongoose.Schema({
+    name: String,
+    phone: String,
+    email: String,
+    address: String,
+    des: String
+
+});
+
+const Contact = mongoose.model('Contact', contactSchema);
+
+
+
+
+// EXPRESS SPECIFIC STUFF
+app.use('/static', express.static('static')) // For serving static files
+app.use(express.urlencoded())
+
+// PUG SPECIFIC STUFF
+app.set('view engine', 'pug') // Set the template engine as pug
+app.set('views', path.join(__dirname, 'views')) // Set the views directory
+ 
+// ENDPOINTS
+app.get('/', (req, res)=>{
+    const con = "This is the best content on the internet so far so use it wisely"
+    const params = { }
+    res.status(200).render('index.pug', params);
+})
+app.get('/home', (req, res)=>{
+    const con = "This is the best content on the internet so far so use it wisely"
+    const params = { }
+    res.status(200).render('home.pug', params);
+})
+app.get('/contact',(req,res)=>{
+    res.status(200).render('contact.pug');
+})
+app.post('/contact',(req,res)=>{
+    var myData = new Contact(req.body);
+    myData.save().then(()=>{
+        res.render("contact.pug");
+    }).catch(()=>{
+        res.status(400).send("item was not saved to the databse");
+    });
+    
+})
+
+
+// START THE SERVER
+app.listen(port, ()=>{
+    console.log(`The application started successfully on port ${port}`);
+});
